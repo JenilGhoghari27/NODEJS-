71287dc406e6e6c78273516b39d551e9e6c53682
JenilGhoghari27 <146430199+JenilGhoghari27@users.noreply.github.com>
2024-05-03 10:43:26 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const User = require("../model/User")
-// const joi= require("joi");
 const Friend = require("../model/friend")
-// const bcrypt = require("bcrypt")
-// const jwt = require('jsonwebtoken');
 const jwtprotect = require('../controllers/protect')
 const UserController = require("../controllers/protect")
 const multer = require('multer')
-
-
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 
 ////// USER ///////
 
@@ -27,10 +16,8 @@ router.get('/find', UserController.protect, UserController.find);  //VIEW
 
 router.delete('/delete', UserController.delete); //DLT
 
-
-
-
 //////////// FRIEND /////////////////
+
 router.post('/ADD', UserController.ADD);
 
 router.get('/ALL', jwtprotect.protect, UserController.ALL)
